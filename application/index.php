42ba1b5962da1bfdbaea790836d6e02c7e64d0f7
Rami <ramiro.alvarez@vasslatam.com>
2020-11-05 00:58:37 -0500

    sample

---
+++ application/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use setasign\Fpdi\PdfParser\CrossReference\CrossReferenceException;
+use setasign\FpdiProtection\FpdiProtection;
+
+$originFile = "IVR_SE.pdf";
+$originFile = "protegido.pdf";
+$destFile = "alternativo/".$originFile;
+$pass = "123";
+
+$pdf = new FpdiProtection();
+$alreadyProtected = False;
+
+try {
+    $ownerPassword = $pdf->setProtection(
+        array(),
+        $pass,
+        'ABCD'
+    );
+    $pageCount = $pdf->setSourceFile($originFile);
+} catch (CrossReferenceException $error) {
+    $alreadyProtected = True;
+}
+
+if($alreadyProtected) {
+    echo $originFile; /* esto es solo para depurar */
+    return $originFile;
+}
+
+for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+    $templateId = $pdf->importPage($pageNo);
+    $pdf->AddPage();
+    $pdf->useTemplate($templateId, ['adjustPageSize' => true]);
+}
+
+// Output the new PDF
+$pdf->Output('F', $destFile);
+echo $destFile;
+return;
